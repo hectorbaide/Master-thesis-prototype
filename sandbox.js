5668b7f393de745d7165a5829168251a1e61e00c
Hector <hectorbaide@gmail.com>
2013-06-17 15:33:30 +0200

    Data building blocks for 2nd interface
    Arrays objectified and sorted, need to develop graphical front for it

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,5 @@
-function iIT (element, index, array) {
-		var stuff = string;
-		return (element == stuff);
+function iIT (element, index) {
+		return (element == string);
 	}
 
 function svgArray (array, fuckit) {
@@ -8,3 +7,35 @@ function svgArray (array, fuckit) {
 	array.filter(iIT);
 }
 	
+var container = [];
+
+function objectBox (tag, number) {
+	this.tag = tag;
+	this.number = number;
+}
+
+
+function createContainer() {
+	iTR();
+	sortiTR();
+}
+
+function iTR () {
+	container = [];
+	for (var i = tagoloco.length - 1; i >= 0; i--) {
+		string = tagoloco[i];
+		var name = everyTags.filter(iIT);
+		var length = name.length;
+		name = _.uniq(name);
+		name = name.toString();
+		container[i] = new objectBox(name,length);  
+	}
+}
+
+function sortiTR () {
+	container.sort(function (a, b) { return a.number - b.number;
+	});
+	container.reverse();
+}
+
+
