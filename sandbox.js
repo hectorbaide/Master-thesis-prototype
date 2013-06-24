dfbfc72e08e98e1dd4e8c5d7acb345f9649db0cb
Hector <hectorbaide@gmail.com>
2013-06-24 16:40:09 +0200

    New developments
    Started using gRaphael, piechart works

--- sandbox.js
+++ sandbox.js
@@ -39,3 +39,36 @@ function sortiTR () {
 }
 
 
+//RaphaelJS experimentation
+
+var mesa = Raphael(0,0,100%,100%);
+
+function stacker(shape, posx, posy, radius, color, stroke, tag) {
+	this.tag = tag;
+	this.shape = "circle";
+	this.posx = posx;
+	this.posy = posy;
+	this.radius = radius;
+	this.color = color;
+	this.stroke = "none";
+	this.tag = tag;
+}
+
+var stacks = [];
+
+function createStacks () {
+	stacks = [];
+	for (var i = container.length - 1; i >= 0; i--) {
+		var sizer = container[i].number;
+		var positionerx = container[i].number + 50;
+		var positionery = container[i].number + 100;
+		var shape = "circle";
+		var posx = positionerx;
+		var posy = positionery;
+		var radius = sizer;
+		var color = "FFBBAA";
+		var stroke = "none";
+		var tag = container[i].tag;
+		stacks[i] = new stacker(shape, posx, posy, radius, color, stroke, tag) 
+	};
+}
